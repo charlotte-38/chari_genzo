0af6e00924137977827de9f8dcff11bdc67b1f14
charlotte-38 <113467063+charlotte-38@users.noreply.github.com>
2022-09-13 20:16:22 +0200

    Initial commit 2

---
+++ Genzo/Genzo/Display.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Genzo
+{
+    internal class Display
+    {
+
+    }
+
+}
---
+++ Genzo/Genzo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Genzo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck Starter = new Deck();
+            Console.WriteLine(Starter.DeckGetCard());
+            Starter.DeckShuffle();
+            _ = Console.ReadKey();
+        }
+    }
+}
